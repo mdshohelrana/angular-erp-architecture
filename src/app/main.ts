13dff88b3fa3ac13f5b1df6152ec31292574fc7f
Md. Shohel Rana <cse_shohel@yahoo.com>
2017-04-19 14:40:56 +0600

    updated tools setting

--- src/app/main.ts
+++ src/app/main.ts
@@ -3,7 +3,8 @@ import { enableProdMode } from '@angular/core';
 
 import { AppModule } from './app.module';
 
-//enableProdMode(); //Uncomment for production
+// if (String('<%= BUILD_TYPE %>') === 'prod') { enableProdMode(); }
+
 platformBrowserDynamic().bootstrapModule(AppModule)
   .then((success: any) => console.log('App bootstrapped'))
   .catch((err: any) => console.error(err));
---
+++ tools/env/base.ts
@@ -0,0 +1,8 @@
+import { EnvConfig } from './env-config.interface';
+
+const BaseConfig: EnvConfig = {
+  // Sample API url
+  API: 'https://localhost:3000'
+};
+
+export = BaseConfig;
